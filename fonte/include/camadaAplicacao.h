d27d6ed1ec8c7148f0d8d14285ec1e1621b8b0af
Arcoverde <plvaa03@gmail.com>
2023-01-29 18:53:31 -0300

    bibs

--- fonte/include/camadaAplicacao.h
+++ fonte/include/camadaAplicacao.h
@@ -1,2 +1,37 @@
 #include "camadaFisica.h"
 
+const int ASCII_MAX_BITS = 8;
+
+/**
+ * Entrada na camada de aplicação para transmissão.
+ */
+void AplicacaoTransmissora();
+
+/**
+ * Parte da camada de aplicação que transmite dados chamando a camada física.
+ * @param mensagem Mensagem que vai ser codificada na camada física.
+ * @param codificacaoFisica Codificação escolhida, entre Binária, Bipolar e
+ * Manchester.
+ */
+void CamadaDeAplicacaoTransmissora(const string&, int);
+
+/**
+ * Recebe string e retorna um vetor de inteiros dos valores binários em ASCII de
+ * cada caracter.
+ * @param mensagem String a ser convertida.
+ * @return Um vetor com os bits em binário da string em ASCII.
+ */
+vector<int> TransformaStringEmASCII(string);
+
+/**
+ * Recebe vetor de bits em ASCII e converte em string.
+ * @param quadro Um vetor com os bits em binário da string em ASCII.
+ * @return A string convertida.
+ */
+string TransformaASCIIEmString(vector<int>);
+
+/**
+ * Entrada na camada de aplicação para recepção.
+ * @param mensagem String resultado da decodificação.
+ */
+void AplicacaoReceptora(const string&);
